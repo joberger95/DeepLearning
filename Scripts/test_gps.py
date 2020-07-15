a717258437356e5ebfe62c543e4d598b057bbf91
joberger95 <jordan.berger@ynov.com>
2020-07-15 09:13:42 +0200

    maj gps

--- Scripts/test_gps.py
+++ Scripts/test_gps.py
@@ -10,11 +10,12 @@ import adafruit_gps
 # a slightly higher timeout (GPS module typically update once a second)
 # These are the defults you should use for the GPS FeatherWing.
 # For other boards set RX = GPS module TX, and TX = GPS module RX pins
-uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
+# uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
 
 # for a computer use the pyserial library for uart access
-# import serial
-# uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=10)
+import serial
+
+uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=10)
 
 # Creat a GPS module instance
 gps = adafruit_gps.GPS(uart, debug=False)
