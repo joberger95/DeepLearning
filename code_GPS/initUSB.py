02a30c9dc1241514455011e547f121b6ff4a844c
joberger95 <jordan.berger@ynov.com>
2020-07-22 10:37:04 +0200

    switch

--- code_GPS/initUSB.py
+++ code_GPS/initUSB.py
@@ -1,16 +1,8 @@
-import usb.core
+import usb
+import usb.backend.libusb1 as libusb1
 
-dev = usb.core.find(idVendor=0x0403, idProduct=0x6001)
-ep = dev[0].interfaces()[0].endpoints()[0]
-i = dev[0].interfaces()[0].bInterfaceNumber
-dev.reset()
+backend = libusb1.get_backend(find_library=lambda _: "LibUSB/MS64/dll/libusb-1-0.dll")
+dev = usb.core.find(backend=backend)
 
-if dev.is_kernel_driver_active(i):
-    dev.detach_kernel_driver(i)
-
-dev.set_configuration()
-eaddr = ep.bEndpointAddress
-
-r = dev.read(eaddr, 100)
-print(len(r))
-
+print(dev)
+parameter_list
