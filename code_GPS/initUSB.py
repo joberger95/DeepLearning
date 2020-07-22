e14da6c83b5b75c71dafddc76b5968a4af8ebe8d
joberger95 <jordan.berger@ynov.com>
2020-07-22 15:49:47 +0200

    fincd USB

--- code_GPS/initUSB.py
+++ code_GPS/initUSB.py
@@ -1,7 +1,20 @@
-import usb
-import usb.backend.libusb1 as libusb1
+import re
+import subprocess
 
-backend = libusb1.get_backend(find_library=lambda _: "LibUSB/MS64/dll/libusb-1-0.dll")
-dev = usb.core.find(backend=backend)
-
-print(dev)
+device_re = re.compile(
+    "Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$",
+    re.I,
+)
+df = subprocess.check_output("lsusb")
+devices = []
+for i in df.split("\n"):
+    if i:
+        info = device_re.match(i)
+        if info:
+            dinfo = info.groupdict()
+            dinfo["device"] = "/dev/bus/usb/%s/%s" % (
+                dinfo.pop("bus"),
+                dinfo.pop("device"),
+            )
+            devices.append(dinfo)
+print(devices)
