fe840c2db3869a071ebf8db214e3c0427e17ca90
joberger95 <jordan.berger@ynov.com>
2020-07-23 09:56:49 +0200

    Find usb antenna

--- code_GPS/initUSB.py
+++ code_GPS/initUSB.py
@@ -1,6 +1,44 @@
-import usb.core
+import sys
 
-for dev in usb.core.find(find_all=True):
-    print("Device:", dev.filename())
-    print("idVendor: %d (%s)" % (dev.idVendor, hex(dev.idVendor)))
-    print("idProdut: %d (%s)" % (dev.idProduct, hex(dev.idProduct)))
+import usb.core
+import usb.util
+
+print(usb.__version__)
+busses = usb.busses()
+for bus in busses:
+    devices = bus.devices
+    for dev in devices:
+        if dev != None:
+            try:
+                usd_dev = usb.core.find(idVendor=dev.idVendor, idProduct=dev.idProduct)
+                print(usb_dev)
+            except:
+                pass
+# 1a40:0101
+dev = usb.core.find(idVendor=0x0403, idProduct=0x6001)
+print("The 8087:0024 is : ", dev)
+if dev is None:
+    raise ValueError("Device not found!")
+else:
+
+    if dev.is_kernel_driver_active(0):
+        try:
+            dev.detach_kernel_driver(0)
+            print("kernel driver detached")
+        except usb.core.USBError as e:
+            sys.exit("Could not detach kernel driver: %s" % str(e))
+    else:
+        print("no kernel driver attached")
+    try:
+        usb.util.claim_interface(dev, 0)
+        print("claimed device")
+    except:
+        sys.exit("Could not claim the device: %s" % str(e))
+    try:
+        dev.set_configuration()
+        dev.reset()
+    except usb.core.USBError as e:
+        sys.exit("Could not set configuration: %s" % str(e))
+
+usb.util.release_interface(dev, interface)
+dev.attach_kernel(interface)
