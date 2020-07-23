2dba539e533b164d061308b79628996ec54cab51
joberger95 <jordan.berger@ynov.com>
2020-07-23 09:54:26 +0200

    maj

--- code_GPS/initUSB.py
+++ code_GPS/initUSB.py
@@ -1,6 +1,6 @@
 import usb.core
 
 for dev in usb.core.find(find_all=True):
-    print("Device:", dev.filename)
+    print("Device:", dev.filename())
     print("idVendor: %d (%s)" % (dev.idVendor, hex(dev.idVendor)))
     print("idProdut: %d (%s)" % (dev.idProduct, hex(dev.idProduct)))
